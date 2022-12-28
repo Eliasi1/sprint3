381aef190202bd8c1fe7d60dda121ba5fac11056
Eliasi1 <eliasiohad@gmail.com>
2022-12-28 16:51:26 +0200

    just push

--- apps/mail/services/mail.service.js
+++ apps/mail/services/mail.service.js
@@ -1,1 +1,13 @@
-console.log('Hi')
+export const mailService = {
+    getMail
+}
+
+const Mails = [
+    { id: 'e101', subject: 'Miss you!', body: 'Would love to catch up sometimes', isRead: false, sentAt : 1551133930594, to: 'momo@momo.com' },
+    { id: 'e102', subject: 'Thank you for contacting us', body: 'our team in bobMobile will look in this issue and contact you within a year', isRead: false, sentAt : 1551166430594, to: 'momo@momo.com' },
+    { id: 'e103', subject: 'registration failed', body: 'i think you dont have enough money in the account, please find a job and try again', isRead: true, sentAt : 1553433930594, to: 'momo@momo.com' },
+]
+
+function getMail() {
+    return Promise.resolve(Mails)
+}
--- apps/mail/views/mail-index.jsx
+++ apps/mail/views/mail-index.jsx
@@ -1,5 +1,23 @@
 
 export function MailIndex() {
-    return <div>mail app</div>
+    function onCompose(){
+        console.log("compose!")
+    }
+    function onEmailCategorySelect(ev){
+        console.log(ev)
+    }
+
+    return <section className="mail-main">
+        <div className="nav-btn">
+        <button onClick={onCompose} className="glow-on-hover" type="button">Compose</button>   
+        <ul>
+            <li onClick={onEmailCategorySelect}>Inbox</li>
+            <li>Stared</li>
+            <li>Sent Email</li>
+            <li>Draft</li>
+        </ul>
+        </div>
+
+    </section>
 }
 
