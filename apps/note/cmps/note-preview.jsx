4321061db3558a05e0926d7033d0191ad965c234
Shay Skitel <61014430+ShaySkitel@users.noreply.github.com>
2022-12-28 18:54:24 +0200

    Show buttons when you hover on NotePreview

--- apps/note/cmps/note-preview.jsx
+++ apps/note/cmps/note-preview.jsx
@@ -2,5 +2,8 @@ export function NotePreview({ note }) {
     return <article className="note-card">
         <h3>{note.info.title}</h3>
         <p>{note.info.txt}</p>
+        <section className="note-buttons">
+            <button className="fa-solid trash-can"></button>
+        </section>
     </article>
 }
