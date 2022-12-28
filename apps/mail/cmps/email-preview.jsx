500d9536ddeff7dc4db5207ac77efa09c4c6314f
Eliasi1 <eliasiohad@gmail.com>
2022-12-29 01:14:47 +0200

    end of day 1

--- apps/mail/cmps/email-preview.jsx
+++ apps/mail/cmps/email-preview.jsx
@@ -10,7 +10,7 @@ export function MailPreview ({mail, onStarMail, onRemoveMail}){
     return <Fragment>
     <div className="mail-preview mail-actions">
     <button onClick={() => onRemoveMail(mail.id)} className="fa-solid trash-can"></button>
-    <button onClick={() => onStarMail(mail.id)} className="fa-solid star"></button>
+    <button onClick={() => onStarMail(mail.id)} className={(mail.isStarred)? "fa-solid star" : "fa-regular star" } ></button>
     </div>
     <h2 className='mail-preview subject'>{mail.subject}</h2>
     <h2 className='mail-preview time-stamp'>{utilService.getFullDate(mail.sentAt)}</h2>
