fd02320d8d9b3bd0f6104fafd71daab92f98cb68
Shay Skitel <61014430+ShaySkitel@users.noreply.github.com>
2022-12-28 17:38:32 +0200

    Add closing add-note form on body click

--- apps/note/cmps/note-add.jsx
+++ apps/note/cmps/note-add.jsx
@@ -1,16 +1,33 @@
-const { useState } = React
+const { useState, useEffect } = React
 
 export function NoteAdd() {
 
     const [isAddingNote, setIsAddingNote] = useState(false)
 
+    useEffect(() => {
+        document.body.addEventListener('click', handleBodyClick)
+
+        return () => {
+            document.body.removeEventListener('click', handleBodyClick)
+        }
+    }, [])
+
+    function handleBodyClick(ev) {
+        setIsAddingNote(false)
+    }
+
+    function onOpenForm(ev) {
+        ev.stopPropagation()
+        setIsAddingNote((prevIsAddingNote => !prevIsAddingNote))
+    }
+
     return <section className="note-add">
-        {!isAddingNote && <input onClick={() => setIsAddingNote((prevIsAddingNote => !prevIsAddingNote))} type="text" placeholder="Take a note..." />}
+        {!isAddingNote && <input onClick={onOpenForm} type="text" placeholder="Take a note..." />}
         {isAddingNote && <section className="note-add-section">
             <input type="text" placeholder="Title" />
             <input type="text" placeholder="Take a note..." />
             <div>
-                <button onClick={() => setIsAddingNote((prevIsAddingNote => !prevIsAddingNote))}>Close</button>
+                <button onClick={onOpenForm}>Close</button>
             </div>
         </section>}
     </section>
