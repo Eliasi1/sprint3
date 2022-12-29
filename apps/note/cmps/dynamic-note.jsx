66a77542500712fde6dcfad2f110e8a1674f6ad2
Eliasi1 <eliasiohad@gmail.com>
2022-12-29 14:26:44 +0200

    submission 14:00

---
+++ apps/note/cmps/dynamic-note.jsx
@@ -0,0 +1,17 @@
+import { NoteImg } from "./note-img.jsx"
+import { NoteTodos } from "./note-todos.jsx"
+import { NoteTxt } from "./note-txt.jsx"
+import { NoteVideo } from "./note-video.jsx"
+
+export function DynamicNote({ note }) {
+    switch (note.type) {
+        case 'note-txt':
+            return <NoteTxt note={note} />
+        case 'note-img':
+            return <NoteImg note={note} />
+        case 'note-video':
+            return <NoteVideo note={note} />
+        case 'note-todos':
+            return <NoteTodos note={note} />
+    }
+}
