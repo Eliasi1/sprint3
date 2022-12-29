989bba7f61f0f92a10d25aec5522f722239d238e
Shay Skitel <61014430+ShaySkitel@users.noreply.github.com>
2022-12-29 11:45:05 +0200

    Change note to be a dynamic note

--- apps/note/cmps/note-preview.jsx
+++ apps/note/cmps/note-preview.jsx
@@ -1,13 +1,14 @@
+import { DynamicNote } from "./dynamic-note.jsx"
+
 export function NotePreview({ note, onRemoveNote, onOpenModal }) {
-    function handleModalOpening(ev){
-        if(ev.target.parentElement.classList.contains('note-buttons')) return
+    function handleModalOpening(ev) {
+        if (ev.target.parentElement.classList.contains('note-buttons')) return
         onOpenModal(note)
     }
 
 
     return <article onClick={handleModalOpening} className="note-card">
-        <h3>{note.info.title}</h3>
-        <p>{note.info.txt || 'Empty note'}</p>
+        <DynamicNote note={note} />
         <section className="note-buttons">
             <button onClick={() => onRemoveNote(note.id)} className="fa-solid trash-can"></button>
         </section>
