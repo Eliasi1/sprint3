36035fdfacb6509e678b22dc1d04ead6368d6492
Shay Skitel <61014430+ShaySkitel@users.noreply.github.com>
2022-12-30 16:51:06 +0200

    Add button to compose an email with the note's content

--- apps/note/cmps/note-preview.jsx
+++ apps/note/cmps/note-preview.jsx
@@ -1,6 +1,11 @@
+const {useNavigate} = ReactRouterDOM
+
 import { DynamicNote } from "./dynamic-note.jsx"
 
 export function NotePreview({ note, onRemoveNote, onOpenModal, onChangeColor, onPinNote }) {
+
+    const navigate = useNavigate()
+
     function handleModalOpening(ev) {
         if (ev.target.parentElement.classList.contains('note-buttons') || ev.target.parentElement.classList.contains('color-button')) return
         onOpenModal(note)
@@ -11,10 +16,17 @@ export function NotePreview({ note, onRemoveNote, onOpenModal, onChangeColor, on
         onChangeColor(note, value)
     }
 
+    function onNoteToMail(){
+        const subject = note.info.title
+        const body = note.info.txt || note.info.url || note.info.todos.map(todo => todo.txt)
+        navigate(`/mail/compose?subject=${subject}&body=${body}`)
+    }
+
     return <article onClick={handleModalOpening} style={note.style ? note.style : {}} className="note-card">
         <DynamicNote note={note} />
         <section className="note-buttons">
             <button onClick={() => onPinNote(note)} className={"fa-solid thumbtack pin-button " + (note.isPinned ? 'pinned' : '')}></button>
+            <button onClick={onNoteToMail} className="fa-solid envelope"></button>
             <button className="fa-solid palette color-button"><input value={note.style ? note.style.backgroundColor : '#ffffff'} onChange={handleColorChange} type="color" /></button>
             <button onClick={() => onRemoveNote(note.id)} className="fa-solid trash-can"></button>
         </section>
