2d37acd0c433b51c385e0a298f464229842f6039
Shay Skitel <61014430+ShaySkitel@users.noreply.github.com>
2022-12-31 12:03:43 +0200

    Change homepage

--- views/home.jsx
+++ views/home.jsx
@@ -1,7 +1,14 @@
+const { useNavigate } = ReactRouterDOM
 
 export function Home() {
 
+    const navigate = useNavigate()
+
     return <section className="home">
-        <h1>Welcome to home page!</h1>
+        <h1>Welcome to Appsus</h1>
+        <div className="image-container">
+            <div title="Go to mails" onClick={() => navigate('/mail/inbox')} className="img"><img src="../assets/img/mail.svg" alt="mail image" /></div>
+            <div title="Go to notes" onClick={() => navigate('/note')} className="img"><img src="../assets/img/notes.svg" alt="notes image" /></div>
+        </div>
     </section>
 }
